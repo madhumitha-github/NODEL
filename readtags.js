798fe374ce7969a0132d6a9bdb0bb287f2fcfe98
madhumitha-github <madhu.22.mitha@gmail.com>
2016-07-03 02:44:54 +0530

    Update readtags.js

--- readtags.js
+++ readtags.js
@@ -1,13 +1,12 @@
 
-var url = require('url');
 var express = require('express');
 var Clarifai = require('clarifai');
 var appServer = express();
 var port = process.env.PORT || 8080;
 
 Clarifai.initialize({
-    'clientId': 'hERl8Zvb63fJpRtpCwp1bGP_7JiyCfwUqlzfq2kS',
-    'clientSecret': 'LQDwDlvFV6jKr9F0Gw2NvUHv3pUtKFsfQmXeY00q'
+    'clientId': '',
+    'clientSecret': ''
 });
 
 appServer.get('/', function(serverRequest, serverResponse) {
