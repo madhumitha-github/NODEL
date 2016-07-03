1af5f581c9899edee265d95cb512fec72b7540a3
madhumitha-github <madhu.22.mitha@gmail.com>
2016-07-03 15:18:11 +0530

    Update readtags.js

--- readtags.js
+++ readtags.js
@@ -5,8 +5,8 @@ var appServer = express();
 var port = process.env.PORT || 8080;
 
 Clarifai.initialize({
-    'clientId': '',
-    'clientSecret': ''
+    'clientId': process.env.CLIENT_ID,
+    'clientSecret': process.env.CLIENT_SECRET
 });
 
 appServer.get('/', function(serverRequest, serverResponse) {
@@ -14,13 +14,11 @@ appServer.get('/', function(serverRequest, serverResponse) {
 });
 
 appServer.get('/getTags', function(serverRequest, serverResponse) {
-    
     var imageurl = serverRequest.param('url');
     if(imageurl == undefined) {
         return serverResponse.send('Expected url format => htp://host:name/getTags?url=image_url');
     }
-  	        
-    Clarifai.getTagsByUrl(imageurl).then(
+  	Clarifai.getTagsByUrl(imageurl).then(
   	    function(response) {
   	        serverResponse.json({'Tags' : response["results"][0].result["tag"]["classes"]});
         },
