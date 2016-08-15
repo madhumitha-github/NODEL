17fe0791d11c56a994d6494ab39f0be5e0abe016
madhumitha-github <madhu.22.mitha@gmail.com>
2016-08-15 16:42:43 +0530

    Update invokeservice.js

--- invokeservice.js
+++ invokeservice.js
@@ -3,13 +3,12 @@ var http = require('http');
 
 var options = {
   host: 'https://hu4xwdeme9.execute-api.us-east-1.amazonaws.com',
-  https://hu4xwdeme9.execute-api.us-east-1.amazonaws.com/DEV/bankingbot/accountsummary?userKey=1000&accountType=CHECKING
   path: '/DEV/bankingbot/accountsummary?userKey=1000&accountType=CHECKING',
   method: 'GET'
 };
 
 http.request(options, function(response) {
-  response.on('data', function (dat) {
-    console.log('BODY: ' + dat);
+  response.on('data', function (summary) {
+    console.log('Response: ' + summary);
   });
 }).end();
